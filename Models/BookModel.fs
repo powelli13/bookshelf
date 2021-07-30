ae904afa585454ff1abc2a3dce19eee546cc9b90
Isaac Powell <powelli91@outlook.com>
2021-07-29 22:53:23 -0700

    deleted F# ASP.NET app

--- Models/BookModel.fs
+++
@@ -1,9 +0,0 @@
-namespace Bookshelf.Models
-
-open System
-
-type BookModel (title:string, author:string, yearPublished:Nullable<int>) =
-    member val Title = title with get, set
-    member val Author = author with get, set
-    member val YearPublished = yearPublished with get, set
-    new () = BookModel("", "", 1900)
